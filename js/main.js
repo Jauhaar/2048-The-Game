45dbd9f6e82de53f94701c072e48406eb1bac516
Mohammed Jauhaar Saib <m.jauhaar.saib@gmail.com>
2020-01-07 23:50:04 +0200

    Worked on optimisation, modularisation and code clean up. Made progress on the Move function.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-// TODO: Implement movement
-// TODO: Implement score tracking
+// TODO: Implement movement (WORKING ON)
 // TODO: Implement on screen controls
+// TODO: Implement score tracking
 // TODO: Implement restart
 //
-// DONE: Load game with two 2's or one 2 and one 4 in random cells
-
-// change the class of the child of each cell
+// DONE: Game Initialisation
 
 var gameBoard = document.querySelector('game-board-grid');
 // Cells
@@ -35,31 +33,8 @@ window.addEventListener('load', InitialiseGame());
 
 document.addEventListener('keyup', function(e) {
   console.log(e.key);
-  // Move('right');
+  // Game Interaction takes place here
 });
-
-function Move(dir) {
-  let direction = dir;
-  let canMove;
-  let row1 = [cell_1_1, cell_2_1, cell_3_1, cell_4_1];
-
-  row1.forEach(cell => {
-    cellChildClassList = cell.firstElementChild.classList;
-    siblingChildClassList = cell.nextElementSibling.firstElementChild.classList;
-    if (cell != row1[3] && siblingChildClassList.contains('empty')) {
-      canMove = true;
-    } else {
-      canMove = false;
-    }
-
-    if (canMove) {
-      cell.firstElementChild.classList.remove('num-2');
-      cell.firstElementChild.classList.add('empty');
-      cell.nextElementSibling.firstElementChild.classList.remove('empty');
-      cell.nextElementSibling.firstElementChild.classList.add('num-2');
-    }
-  });
-}
 
 function InitialiseGame() {
   let cell1 = GetRandomCell();
@@ -75,22 +50,62 @@ function InitialiseGame() {
   }
 
   // Cell1 is always 2
-  SetCell(cell1, 'empty', 'num-2', '2');
+  cell1.replaceChild(Node('2'), cell1.firstElementChild);
   // Cell2 is either 2 or 4
   const random = Random(0, 2);
   if (random == 0) {
-    SetCell(cell2, 'empty', 'num-2', '2');
+    cell2.replaceChild(Node('2'), cell2.firstElementChild);
   } else {
-    SetCell(cell2, 'empty', 'num-4', '4');
+    cell2.replaceChild(Node('4'), cell2.firstElementChild);
   }
+
+  // LEGACY
+
+  // // Cell1 is always 2
+  // SetCell(cell1, 'empty', 'num-2', '2');
+  // // Cell2 is either 2 or 4
+  // const random = Random(0, 2);
+  // if (random == 0) {
+  //   SetCell(cell2, 'empty', 'num-2', '2');
+  // } else {
+  //   SetCell(cell2, 'empty', 'num-4', '4');
+  // }
 }
 
-function SetCell(cell, removeClassName, addClassName, value = null) {
-  element = cell.firstElementChild;
-  element.classList.remove(removeClassName);
-  element.classList.add(addClassName);
-  element.textContent = value;
+function Move() {
+  let row1 = [cell_1_1, cell_2_1, cell_3_1, cell_4_1];
+  var count = row1.length * row1.length;
+  let canMove;
+
+  // Main Loop
+  for (let index = 0; index < count; index++) {
+    const cell = row1[index % row1.length]; //makes it loop through the row 4 times
+    // console.log(cell);
+
+    var cellChildClassList = cell.firstElementChild.classList;
+    var siblingChildClassList =
+      cell.nextElementSibling.firstElementChild.classList;
+
+    // CAN BE MADE INTO SEPERATE FUNCTION
+    if (cellChildClassList.contains('empty')) {
+      canMove = false;
+    } else {
+      if (cell != row1[3] && siblingChildClassList.contains('empty')) {
+        canMove = true;
+      } else {
+        canMove = false;
+      }
+    }
+
+    if (canMove) {
+      cell.firstElementChild.classList.remove('num-2');
+      cell.firstElementChild.classList.add('empty');
+      cell.nextElementSibling.firstElementChild.classList.remove('empty');
+      cell.nextElementSibling.firstElementChild.classList.add('num-2');
+    }
+  }
 }
+// var test = Move();
 
 function GetRandomCell() {
   let cell = null;
@@ -144,4 +159,67 @@ function Random(min, max) {
   var randomNumber = Math.floor(Math.random() * (max - min) + min);
 
   return randomNumber;
+}
+
+// This function creates the node that will replace existing nodes in cells
+function Node(className) {
+  // Example of how to replace a node in a cell using this function:
+  // cell_1_1.replaceChild(Node('2048'), cell_1_1.firstElementChild);
+  if (className == 'empty') {
+    var node = document.createElement('div');
+    node.classList = `cell ${className}`;
+  } else {
+    var node = document.createElement('div');
+    node.classList = `cell num-${className}`;
+    var textNode = document.createTextNode(className);
+    node.appendChild(textNode);
+  }
+
+  return node;
+}
+
+// LEGACY FUNCTION USE: Move()
+function LegacyMove(dir) {
+  let direction = dir;
+  let canMove;
+  let row1 = [cell_1_1, cell_2_1, cell_3_1, cell_4_1];
+  row1.forEach(cell => {
+    var cellChildClassList = cell.firstElementChild.classList;
+    var siblingChildClassList =
+      cell.nextElementSibling.firstElementChild.classList;
+
+    // CAN BE MADE INTO SEPERATE FUNCTION
+    if (cellChildClassList.contains('empty')) {
+      console.log('cant move');
+    } else {
+      if (cell != row1[3] && siblingChildClassList.contains('empty')) {
+        canMove = true;
+      } else {
+        canMove = false;
+      }
+    }
+
+    if (canMove) {
+      cell.firstElementChild.classList.remove('num-2');
+      cell.firstElementChild.classList.add('empty');
+      cell.nextElementSibling.firstElementChild.classList.remove('empty');
+      cell.nextElementSibling.firstElementChild.classList.add('num-2');
+    }
+  });
+}
+
+// LEGACY FUNCTION USE: Node()
+function SetCell(cell, removeClassName, addClassName, value = null) {
+  element = cell.firstElementChild;
+  element.classList.remove(removeClassName);
+  element.classList.add(addClassName);
+  element.textContent = value;
+}
+
+function ModTest(count) {
+  var modArray = [1, 2, 3, 4, 5];
+  for (let index = 0; index < count; index++) {
+    const element = modArray[index % modArray.length];
+    console.log(element);
+  }
 }
